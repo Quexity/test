2aee7c742a707d50e600a71af0824c0059fd90b9
Quexity <tobias.walking@outlook.com>
2025-06-19 18:09:14 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
-<template>
-  <main class="flex">
-    <div class="relative">
-      <div class="absolute top-8 left-8 bg-zinc-600 rounded-lg">
-        <div class="text-xl font-bold p-4">
-          <div class="ml-auto bg-amber-600 rounded-lg h-8 w-8">
-            <div class="flex">
-              <div class="text-white mx-auto">
-                {{ count }}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <button @click="count++" class = "mx-auto hover:cursor-pointer bg-amber-300">
 
-      This is a button    </button>
-  </main>
+<style type="text/tailwindcss">
+/* Colours */
+@theme {
+  --color-beige: #F5F5DC;
+}
+
+/* Common */
+
+
+
+</style >
+
+<template>
+<head>
+  <title>Project</title>
+</head>
+
+<body class="bg-beige flex">
+  <div class= "bg-zinc-200 h-20 w-60 rounded-2xl"></div>
+</body>
 </template>
 
 <script>
